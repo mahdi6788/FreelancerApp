12381f4f4eb2bc114667f78123657f4b7f383972
mahdi <mjalalimech@gmail.com>
2024-11-08 17:54:37 +0400

    textField

--- src/feature/authentication/SendOTPForm.jsx
+++ src/feature/authentication/SendOTPForm.jsx
@@ -1,25 +1,19 @@
 import { useState } from "react";
+import TextField from "../../ui/TextField";
 
 function SendOTPForm() {
-  const [phoneNumber, setPhoneNumber] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <div>
       <form className="space-y-8">
-        <div>
-          <label className="mb-1" htmlFor="phonenumber">
-            شماره موبایل
-          </label>
-          <input
+        <TextField
+          label={" شماره موبایل"}
+          name={"phonenumber"}
           onChange={(e) => setPhoneNumber(e.target.value)}
           value={phoneNumber}
-            id="phonenumber"
-            type="text"
-            className="textField__input"
-          />
-        </div>
-        <button className="btn btn--primary w-full">
-          ارسال کد تایید
-          </button>
+        />
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );

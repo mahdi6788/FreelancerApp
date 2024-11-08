2d83c6dd672d8fd4b9e3d317399ce41a09b9a6da
mahdi <mjalalimech@gmail.com>
2024-11-08 18:51:00 +0400

    use tanstack query

--- src/feature/authentication/SendOTPForm.jsx
+++ src/feature/authentication/SendOTPForm.jsx
@@ -3,17 +3,21 @@ import TextField from "../../ui/TextField";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  
+  const sendOtpHandler = e => {
+    e.preventDefault()
+  }
 
   return (
     <div>
-      <form className="space-y-8">
+      <form className="space-y-8" onSubmit={sendOtpHandler}>
         <TextField
           label={" شماره موبایل"}
           name={"phonenumber"}
           onChange={(e) => setPhoneNumber(e.target.value)}
           value={phoneNumber}
         />
-        <button className="btn btn--primary w-full">ارسال کد تایید</button>
+        <button type="submit" className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );

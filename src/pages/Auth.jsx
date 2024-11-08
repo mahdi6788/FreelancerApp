b3d4154cd7a3dfcd6434e9dbd72947c14ee8c073
mahdi <mjalalimech@gmail.com>
2024-11-08 17:54:49 +0400

    checkotp component

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,16 +1,20 @@
+import CheckOTPForm from "../feature/authentication/CheckOTPForm"
 import SendOTPForm from "../feature/authentication/SendOTPForm"
 
 function Auth() {
   return (
-    <div className=" w-full sm:max-w-sm">
+    <div className="flex justify-center pt-10">
+      <div className=" w-full sm:max-w-sm">
         <SendOTPForm />
+        <CheckOTPForm />
+    </div>
     </div>
   )
 }
 
 export default Auth
 
-/// this is a page for authentication and we put components here, 
+/// into the pages we do not put side effects and just put components that are placed in feature folder. 
 /// containg SendOTPForm and CheckOTPForm
 /// step1: get OTP
 /// step2: check OTP

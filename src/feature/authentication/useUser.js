508700ebade3fc6af47ffbffe41c1d83bc5fdbb5
mahdi <mjalalimech@gmail.com>
2024-11-12 16:10:45 +0400

    make getUser function to get data about user from backend using useQuery

---
+++ src/feature/authentication/useUser.js
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../../services/authService";
+
+export default function useUser(){
+    return useQuery({
+        queryKey: ["get-user"],
+        queryFn: getUser,
+        retry: false,
+        refetchOnReconnect:true,
+        refetchOnWindowFocus: true
+    })
+}
--- src/services/authService.js
+++ src/services/authService.js
@@ -12,5 +12,9 @@ export function completeProfile(data){
     return http.post("/user/complete-profile", data).then(({data}) => data.data)
 }
 
+export function getUser(){
+    return http.get("/user/profile").then(({data}) => data.data)
+}
+
 
 

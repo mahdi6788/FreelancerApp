1368ed79e859b01d294719f352f80b009bc51436
mahdi <mjalalimech@gmail.com>
2024-11-12 16:09:54 +0400

    create global components for header and sidebar into ui folder

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import useUser from "../feature/authentication/useUser"
+
+function Header() {
+  const {data} = useUser()
+  console.log(data)
+  return (
+    <div className="bg-secondary-0 py-4 px-8">header</div>
+  )
+}
+
+export default Header

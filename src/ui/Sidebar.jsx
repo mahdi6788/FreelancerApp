6d8974427ce062f1c37114367e150cdb7cf278f6
mahdi <mjalalimech@gmail.com>
2024-11-13 13:35:09 +0400

    complete sidebar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,8 +1,41 @@
+import { HiCollection, HiHome, HiOutlineCollection } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <div className="bg-secondary-0 row-start-1 row-span-2 ">sidebar</div>
-  )
+    <div className="bg-secondary-0 row-start-1 row-span-2 border-l border-gray-200 p-4">
+      <ul className="flex flex-col gap-y-4 pt-4">
+        <li>
+          <NavLink
+            to="/owner/dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center gap-x-2 px-2 py-1.5 bg-primary-700/50 text-primary-900"
+                : "flex items-center gap-x-2 px-2 py-1.5 hover:text-primary-900 hover:bg-primary-100/50 rounded-lg text-secondary-600 transition-all duration-300"
+            }
+          >
+            <HiHome />
+            <span>داشبورد</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/owner/projects"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center gap-x-2 px-2 py-1.5 bg-primary-700/50 text-primary-900"
+                : "flex items-center gap-x-2 px-2 py-1.5 hover:text-primary-900 hover:bg-primary-100/50 rounded-lg text-secondary-600 transition-all duration-300"
+            }
+          >
+            <HiOutlineCollection />
+            <span>پروژه ها </span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
+
+// hover:text-primary-900 hover:bg-primary-100/50 rounded-lg text-secondary-600 transition-all duration-300

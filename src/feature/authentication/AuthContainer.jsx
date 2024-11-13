e0eec591accd94ecde804e1b9bc9c204b849f527
mahdi <mjalalimech@gmail.com>
2024-11-13 15:34:00 +0400

    delete the default  number

--- src/feature/authentication/AuthContainer.jsx
+++ src/feature/authentication/AuthContainer.jsx
@@ -27,7 +27,7 @@ function AuthContainer() {
   };
 
   /// we need phone number for both sendOtpForm and CheckOtpForm
-  const [phoneNumber, setPhoneNumber] = useState("09123456789");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   /// step is counter for each stage : step1: sending phone number and step2: typing the took code to check
   const [step, setStep] = useState(1);

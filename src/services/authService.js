c5b14b258280f05934504b83c66e48a88bcc2d03
mahdi <mjalalimech@gmail.com>
2024-11-16 14:54:58 +0400

    comment on what happend

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,18 +5,18 @@ import http from "./httpService"
 export function getOtp(data) {
     return http.post("/user/get-otp", data).then(({data})=>data.data)   /// note 13
 }
-
 export function checkOtp(data){
     return http.post("/user/check-otp", data).then(({data})=>data.data) /// note 13
 }
-
 export function completeProfile(data){
     return http.post("/user/complete-profile", data).then(({data}) => data.data)
 }
-
 export function getUser(){
     return http.get("/user/profile").then(({data}) => data.data)
 }
 
 
 
+// getOtp(), checkOtp() and completeProfile() use post to send data to backend (phoneNumber, OTP and userInfo respectively)
+// and get response from backend using .then(). 
+// but in the getUser(), get data from backend directly at first.

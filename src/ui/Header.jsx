f544bd57ec416acd7fba074be81706ce10509e9b
mahdi <mjalalimech@gmail.com>
2024-11-18 11:50:34 +0400

    comment

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,7 +2,7 @@ import useUser from "../feature/authentication/useUser"
 
 function Header() {
   const {data} = useUser()
-  console.log(data)
+  // console.log(data)
   return (
     <div className="bg-secondary-0 py-4 px-8">header</div>
   )

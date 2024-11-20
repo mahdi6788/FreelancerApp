d953084092964162e3d6de208946bafdbd1ab109
mahdi <mjalalimech@gmail.com>
2024-11-20 13:39:50 +0400

    add some comments

--- src/feature/projects/ToggleProjectStatus.jsx
+++ src/feature/projects/ToggleProjectStatus.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
 import Loading from "../../ui/Loading";
 import usetoggleProjectStatus from "./usetoggleProjectStatus";
 import Toggle from "../../ui/Toggle";
 import { Label } from "@headlessui/react";
 
 function ToggleProjectStatus({ project }) {
-  
   const { isUpdating, toggleProjectStatus } = usetoggleProjectStatus();
+
   const toggleHandler = () => {
+    /// when toggling, status changes so should send this change to backend
+    /// so we need a custome hook (usetoggleProjectStatus) to the http method as a function
+    /// this method needs id and data that is status here to send to backend.
     const status = project.status === "OPEN" ? "CLOSE" : "OPEN";
 
     toggleProjectStatus(
       {
         id: project._id,
         data: { status },
-      },
+      }
     );
   };
 
   return (
-    <div className="w=[5rem]">
+    /// to have fix width, set w-[5rem]
+    <div className="w-[5rem]"> 
       {isUpdating ? (
         <Loading height={20} width={50} />
       ) : (

e26f8aceb0025ff6f8d9992db4889c8b47a1019f
mahdi <mjalalimech@gmail.com>
2024-11-20 13:41:37 +0400

    make className dynamic

--- src/ui/Toggle.jsx
+++ src/ui/Toggle.jsx
@@ -7,9 +7,9 @@ function Toggle({enabled, onChange, label}) {
       <Switch
         checked={enabled}
         onChange={onChange}
-        className="group inline-flex h-6 w-11 items-center rounded-full bg-secondary-500 transition data-[checked]:bg-primary-900"
+        className={`group inline-flex h-6 w-11 items-center rounded-full bg-secondary-500 transition ${enabled && "bg-primary-900"}`}
       >
-        <span className="size-4 -translate-x-1 rounded-full bg-secondary-0 transition group-data-[checked]:-translate-x-6" />
+        <span className={`size-4 -translate-x-1 rounded-full bg-secondary-0 transition ${enabled && "-translate-x-6"}`} />
       </Switch>
     </Field>
   );

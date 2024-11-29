c64e708b02c01a88760319564015ce9b6ccd3ca4
mahdi <mjalalimech@gmail.com>
2024-11-29 11:32:25 +0400

    make routes for freelancer and admin just like owner

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,16 @@ import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import OwnerLayout from "./feature/owner/OwnerLayout";
+import FreelancerLayout from "./feature/freelancer/FreelancerLayout";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
+import SubmittedProjects from "./pages/SubmittedProjects";
+import Proposals from "./pages/Proposals";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ProtectedRoute from "./ui/ProtectedRoute";
+import AdminLayout from "./feature/admin/AdminLayout";
+import AdminDashboard from "./pages/AdminDashboard";
+
+import Users from "./pages/Users";
 
 const queryClient = new QueryClient();
 
@@ -19,17 +29,54 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <Toaster />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/complete-profile" element={<CompleteProfile />} />
-
-          <Route path="/owner" element={<OwnerLayout />}>
+          {/* owner */}
+          <Route
+            path="/owner"
+            element={
+              <ProtectedRoute>
+                <OwnerLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate to="dashboard" replace />} />
             <Route path="dashboard" element={<OwnerDashboard />} />
             <Route path="projects" element={<Projects />} />
             <Route path="projects/:id" element={<Project />} />
+          </Route>
+          {/* Freelancer */}
+          <Route
+            path="/freelancer"
+            element={
+              <ProtectedRoute>
+                <FreelancerLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<FreelancerDashboard />} />
+            <Route path="proposals" element={<Proposals />} />
+            <Route path="projects" element={<SubmittedProjects />} />
+          </Route>
+          {/* Admin */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="proposals" element={<Proposals />} />
+            <Route path="projects" element={<SubmittedProjects />} />
+            <Route path="users" element={<Users/>}/>
           </Route>
 
           <Route path="*" element={<NotFound />} />

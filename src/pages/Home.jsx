9eed9064c1fe95241caca06691273156fb9ab1af
mahdi <mjalalimech@gmail.com>
2024-11-29 17:57:24 +0400

    make button to navigate to relative pages also add button on header and disable dashboard there

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
+import { HiHome } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import HeaderMenu from "../ui/HeaderMenu";
+
 function Home() {
+  const navigate = useNavigate()
+
   return (
     <div className="h-screen bg-secondary-0">
       <div className="container xl:max-w-screen-xl">
-        <h1 className="p-4 text-xl text-secondary-700">صفحه اصلی</h1>
+        <div className="flex justify-between">
+        <h1 className="p-4 text-xl text-primary-700 font-extrabold"> Wellcome </h1>
+        <HeaderMenu disabled={false}/>
+        </div>
+        <div className="mt-16">
+          <ul className="flex flex-col items-center gap-y-5 md:flex-row md:justify-around md:gap-x-20 ">
+            <li className="flex justify-center w-full p-5 border border-secondary-0 bg-secondary-200 text-primary-900 font-bold text-lg shadow-lg hover:shadow-blue-100" onClick={() => navigate("/admin")}>
+              ادمین
+            </li>
+            <li className="flex justify-center w-full p-5 border border-secondary-0 bg-secondary-200 text-primary-900 font-bold text-lg shadow-lg hover:shadow-blue-100" onClick={() => navigate("/freelancer")}>
+              فریلنسر
+            </li>
+            <li className="flex justify-center w-full p-5 border border-secondary-0 bg-secondary-200 text-primary-900 font-bold text-lg shadow-lg hover:shadow-blue-100" onClick={() => navigate("/owner")}>
+              کارفرما
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );

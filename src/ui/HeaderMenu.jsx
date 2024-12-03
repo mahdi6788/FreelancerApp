07c89f8e50b1f00a097b27a112261b62b32e8288
Mahdi Jalalifar <Mjalalimech@gmail.com>
2024-12-03 18:56:27 +0400

    when there is a user click on the the top button, s/he should be navigated to the relavant page

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,17 +1,26 @@
-import { HiOutlineUser } from "react-icons/hi"
-import { Link } from "react-router-dom"
-import DarkModeToggle from "./DarkModeToggle"
-import Logout from "../feature/authentication/Logout"
+import { HiOutlineUser } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle";
+import Logout from "../feature/authentication/Logout";
+import useUser from "../feature/authentication/useUser";
 
-function HeaderMenu({disabled}) {
+function HeaderMenu() {
+  const { user } = useUser();
   return (
     <ul className="flex items-center gap-x-4">
       <li className="flex">
-        {(disabled) && 
-        <Link to='dashboard' >
-          <HiOutlineUser className="w-5 h-5 text-primary-900"/>
-        </Link>
-        }
+        {(user?.status===2 && user?.role==="FREELANCER") &&
+        (
+          <Link to="/freelancer/dashboard">
+            <HiOutlineUser className="w-5 h-5 text-primary-900" />
+          </Link>
+        )}
+        {(user?.status===2 && user?.role==="OWNER") &&
+        (
+          <Link to="/owner/dashboard">
+            <HiOutlineUser className="w-5 h-5 text-primary-900" />
+          </Link>
+        )}
       </li>
       <li className="flex">
         <DarkModeToggle />
@@ -20,7 +29,7 @@ function HeaderMenu({disabled}) {
         <Logout />
       </li>
     </ul>
-  )
+  );
 }
 
-export default HeaderMenu
+export default HeaderMenu;

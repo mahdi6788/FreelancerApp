43300d8a0b1e2507f3285ef525c6b47263572ff0
Mahdi Jalalifar <100449011+mahdi6788@users.noreply.github.com>
2024-12-15 22:00:38 +0400

    Update httpService.js
    to connect the frontend to backend that is deployed on render website

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+/// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://freelancerbackend-7jg0.onrender.com";
 
 const app = axios.create({
   baseURL: BASE_URL,
